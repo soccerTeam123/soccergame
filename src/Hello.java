148c8832f05113f6709ddd554590a96d2af3d360
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-03-27 11:44:29 +0900

    변수 유효범위, 데이터 타입

--- src/Hello.java
+++ src/Hello.java
@@ -4,11 +4,13 @@ public class Hello {
 
     public static void main(String[] args) {
 
+
+
         System.out.println("안녕하세요!");
 
-//        String name = "김철수";
+        String name = "김철수";
 
-//        double rn = Math.random();
+        double rn = Math.random();
 
         Scanner sc = new Scanner(System.in);
 
